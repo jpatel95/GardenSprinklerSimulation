27b987d6c8326d02dc9a04ce3bbece96cece027f
nickrinaldi <rinaldinick110@gmail.com>
2016-11-30 12:29:33 -0800

    fixed usage history bug

--- Code/SprinklerSystem/src/com/hummingbee/tests/UsageTester.java
+++ Code/SprinklerSystem/src/com/hummingbee/tests/UsageTester.java
@@ -15,17 +15,44 @@ import com.hummingbee.system.Usage;
 
 public class UsageTester {
 	public static void main(String[] args) {
-		Usage.resetUsages();
+		//Usage.resetUsages();
 		Garden garden = Garden.getInstance();
 		SprinklerCluster northCluster = garden.getCluster(Direction.NORTH);
-		northCluster.activate();
-		try {
-			Thread.sleep(5000);
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		northCluster.deactivate();
+//		northCluster.activate();
+//		try {
+//			Thread.sleep(5000);
+//		}
+//		catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		northCluster.deactivate();
+		
+		System.out.println(northCluster.getId() + ": " + northCluster.getTotalUsage() + "\n");
+		
+//		HashMap<String, LinkedList<DayUsage>> sprinklerUsages = Usage.readSprinklerUsages();
+//		Iterator<String> keyIterator = sprinklerUsages.keySet().iterator();
+//		while (keyIterator.hasNext()) {
+//			String key = keyIterator.next();
+//			LinkedList<DayUsage> usages = sprinklerUsages.get(key);
+//			Iterator<DayUsage> usagesIterator = usages.iterator();
+//			System.out.println(key);
+//			while (usagesIterator.hasNext()) {
+//				DayUsage dayUsage = usagesIterator.next();
+//				System.out.println(dayUsage.getDay().toString() + ": " + dayUsage.getUsage());
+//			}
+//		}
+		
+//		Iterator<Sprinkler> iterator = northCluster.getIterator();
+//		while (iterator.hasNext()) {
+//			Sprinkler sprinkler = iterator.next();
+//			Iterator<DayUsage> usageIterator = sprinkler.getUsageHistory(7).iterator();
+//			System.out.println(sprinkler.getId());
+//			while (usageIterator.hasNext()) {
+//				DayUsage dayUsage = usageIterator.next();
+//				System.out.println(dayUsage.getDay().toString() + ": " + dayUsage.getUsage());
+//			}
+//		}
+		
 	}
 	
 	private static void printMap(HashMap<String, Double> map) {
